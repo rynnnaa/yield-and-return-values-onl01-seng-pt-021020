d5ed4988f673abb3a7d974c69908b2c72d8bbd81
Ryna Truong <rynatruong@outlook.com>
2020-02-22 22:45:38 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -9,5 +9,14 @@ def hello(array)
   end
 end
 
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
